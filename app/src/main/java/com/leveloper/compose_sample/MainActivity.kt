25505a51dbec97d45619b7dab6c9bd466a00bb4f
정상현 <tkdgusl94@gmail.com>
2022-04-10 16:25:11 +0900

    Composable 연습 코드

--- app/src/main/java/com/leveloper/compose_sample/MainActivity.kt
+++ app/src/main/java/com/leveloper/compose_sample/MainActivity.kt
@@ -3,38 +3,84 @@ package com.leveloper.compose_sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.leveloper.compose_sample.ui.theme.Compose_sampleTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            Compose_sampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            var isFavorite by rememberSaveable { mutableStateOf(false) }
+            ImageCard(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp),
+                isFavorite = isFavorite
+            ) {
+                isFavorite = it
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Compose_sampleTheme {
-        Greeting("Android")
+fun ImageCard(
+    modifier: Modifier = Modifier,
+    isFavorite: Boolean,
+    onClickFavorite: (Boolean) -> Unit
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        elevation = 5.dp
+    ) {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "poster",
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopEnd
+            ) {
+                IconButton(onClick = {
+                    onClickFavorite(!isFavorite)
+                }) {
+                    Icon(
+                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "favorite",
+                        tint = Color.White
+                    )
+                }
+            }
+        }
     }
 }
