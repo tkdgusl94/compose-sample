388ab80f556a191dbc731c28bd41c49658d8ae04
정상현 <tkdgusl94@gmail.com>
2022-04-16 22:11:12 +0900

    스탑워치

--- app/src/main/java/com/leveloper/compose_sample/MainActivity.kt
+++ app/src/main/java/com/leveloper/compose_sample/MainActivity.kt
@@ -4,43 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import kotlin.math.pow
+import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : ComponentActivity() {
 
@@ -48,136 +40,164 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val viewModel = viewModel<BmiViewModel>()
-            val navController = rememberNavController()
+            val viewModel = viewModel<MainViewModel>()
 
-            val bmi = viewModel.bmi.value
-
-            NavHost(navController = navController, startDestination = "home") {
-                composable(route = "home") {
-                    HomeScreen { height, weight ->
-                        viewModel.calculateBmi(height, weight)
-
-                        navController.navigate("result")
-                    }
-                }
-                composable(route = "result") {
-                    ResultScreen(navController, bmi)
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun HomeScreen(
-    onClickResult: (Double, Double) -> Unit
-) {
-    val height = rememberSaveable { mutableStateOf("") }
-    val weight = rememberSaveable { mutableStateOf("") }
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = "비만도 계산기") }
-            )
-        }
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            OutlinedTextField(
-                value = height.value,
-                onValueChange = { height.value = it },
-                label = { Text(text = "키") },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-            )
-            OutlinedTextField(
-                value = weight.value,
-                onValueChange = { weight.value = it },
-                label = { Text(text = "몸무게") },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Button(
-                onClick = {
-                    if (height.value.isNotEmpty() && weight.value.isNotEmpty()) {
-                        onClickResult(height.value.toDouble(), weight.value.toDouble())
-                    }
-
+            MainScreen(
+                sec = viewModel.sec.value,
+                milli = viewModel.milli.value,
+                isRunning = viewModel.isRunning.value,
+                lapTimes = viewModel.lapTimes.value,
+                onReset = viewModel::reset,
+                onToggle = { isRunning ->
+                    if (isRunning) viewModel.pause() else viewModel.start()
                 },
-                modifier = Modifier.align(Alignment.End)
-            ) {
-                Text(text = "결과")
-            }
+                onLapTime = viewModel::recodeLapTime
+            )
         }
     }
 }
 
 @Composable
-fun ResultScreen(
-    navController: NavController,
-    bmi: Double
+fun MainScreen(
+    sec: Int,
+    milli: Int,
+    isRunning: Boolean,
+    lapTimes: List<String>,
+    onReset: () -> Unit,
+    onToggle: (Boolean) -> Unit,
+    onLapTime: () -> Unit
 ) {
-    val text = when {
-        bmi >= 35 -> "고도비만"
-        bmi >= 30 -> "2단계 비만"
-        bmi >= 25 -> "1단계 비만"
-        bmi >= 23 -> "과체중"
-        bmi >= 18.5 -> "정상"
-        else -> "저체중"
-    }
-
-    val imageRes = when {
-        bmi >= 23 -> R.drawable.ic_baseline_sentiment_very_dissatisfied_24
-        bmi >= 18.5 -> R.drawable.ic_baseline_sentiment_satisfied_24
-        else -> R.drawable.ic_baseline_sentiment_dissatisfied_24
-    }
-
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text(text = "비만도 계산기") },
-                navigationIcon = {
-                    Icon(
-                        imageVector = Icons.Default.ArrowBack,
-                        contentDescription = "home",
-                        modifier = Modifier.clickable {
-                            navController.popBackStack()
-                        }
-                    )
-                }
-            )
+            TopAppBar(title = { Text(text = "스탑워치") })
         }
     ) {
         Column(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = text, fontSize = 30.sp)
-            Spacer(modifier = Modifier.height(50.dp))
-            Image(
-                painter = painterResource(id = imageRes),
-                contentDescription = null,
-                modifier = Modifier.size(100.dp),
-                colorFilter = ColorFilter.tint(Color.Black)
-            )
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Row(
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Text(text = "$sec", fontSize = 100.sp)
+                Text(text = "$milli")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                lapTimes.forEach { lapTime ->
+                    Text(text = lapTime)
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                FloatingActionButton(
+                    onClick = { onReset() },
+                    backgroundColor = Color.Red
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_baseline_refresh_24),
+                        contentDescription = "reset"
+                    )
+                }
+
+                FloatingActionButton(
+                    onClick = { onToggle(isRunning) },
+                    backgroundColor = Color.Green
+                ) {
+                    Image(
+                        painter = painterResource(id = if (isRunning)
+                            R.drawable.ic_baseline_pause_24
+                        else
+                            R.drawable.ic_baseline_play_arrow_24
+                        ),
+                        contentDescription = "reset"
+                    )
+                }
+
+                Button(onClick = { onLapTime() }) {
+                    Text(text = "랩 타임")
+                }
+            }
         }
     }
 }
 
-class BmiViewModel : ViewModel() {
-
-    private val _bmi = mutableStateOf(0.0)
-    val bmi: State<Double> = _bmi
-
-    fun calculateBmi(
-        height: Double,
-        weight: Double
+@Preview
+@Composable
+fun abc() {
+    MainScreen(
+        sec = 20,
+        milli = 98,
+        isRunning = true,
+        lapTimes = emptyList(),
+        onReset = { /*TODO*/ },
+        onToggle = {}
     ) {
-        _bmi.value = weight / (height / 100.0).pow(2.0)
+
     }
+}
+
+class MainViewModel : ViewModel() {
+    private var time = 0
+    private var timerTask: Timer? = null
+
+    private val _sec = mutableStateOf(0)
+    val sec: State<Int> get() = _sec
+
+    private val _milli = mutableStateOf(0)
+    val milli: State<Int> get() = _milli
+
+    private val _isRunning = mutableStateOf(false)
+    val isRunning: State<Boolean> get() = _isRunning
+
+    private val _lapTimes = mutableStateOf(mutableListOf<String>())
+    val lapTimes: State<List<String>> = _lapTimes
+
+    private var lap = 1
+
+    fun start() {
+        _isRunning.value = true
+
+        timerTask = timer(period = 10) {
+            time++
+            _sec.value = time / 100
+            _milli.value = time % 100
+        }
+    }
+
+    fun pause() {
+        _isRunning.value = false
+        timerTask?.cancel()
+    }
+
+    fun reset() {
+        _isRunning.value = false
+        timerTask?.cancel()
+
+        time = 0
+        _sec.value = 0
+        _milli.value = 0
+        _lapTimes.value.clear()
+        lap = 1
+    }
+
+    fun recodeLapTime() {
+        _lapTimes.value.add(0, "$lap LAP : ${sec.value}.${milli.value}")
+        lap++
+    }
+
 }
